fd90a8701afae5b718f4bb8e180813d77f1f57ed
Tony Won <tonywon89@gmail.com>
2016-03-21 14:55:12 -0700

    Finish subqueries

--- lib/04_select_within_select.rb
+++ lib/04_select_within_select.rb
@@ -28,13 +28,26 @@ def example_select_with_subquery
           countries
         WHERE
           name='Romania'
-        )
+        );
   SQL
 end
 
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      population > (
+        SELECT
+          population
+        FROM
+          countries
+        WHERE
+          name = 'Russia'
+      )
   SQL
 end
 
@@ -42,6 +55,20 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      continent = 'Europe' AND
+      gdp/population > (
+        SELECT
+          gdp/population
+        FROM
+          countries
+        WHERE
+          name = 'United Kingdom'
+      )
   SQL
 end
 
@@ -49,6 +76,19 @@ def neighbors_of_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+    SELECT
+    name, continent
+    FROM
+      countries
+    WHERE
+      continent IN (
+        SELECT
+          continent
+        FROM
+          countries
+        WHERE
+          name IN ('Belize', 'Belgium')
+      )
   SQL
 end
 
@@ -56,6 +96,28 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+    SELECT
+      name, population
+    FROM
+      countries
+    WHERE
+      population >
+        (SELECT
+          population
+        FROM
+          countries
+        WHERE
+          name = 'Canada'
+        )
+      AND
+      population <
+        (SELECT
+          population
+        FROM
+          countries
+        WHERE
+          name = 'Poland'
+        )
   SQL
 end
 
@@ -63,5 +125,18 @@ def sparse_continents
   # Find each country that belongs to a continent where all populations are
   # less than 25,000,000. Show name, continent and population.
   execute(<<-SQL)
+    SELECT
+      name, continent, population
+    FROM
+      countries
+    WHERE
+      continent NOT IN (
+        SELECT
+          DISTINCT continent
+        FROM
+          countries
+        WHERE
+          population > 25000000
+      )
   SQL
 end
